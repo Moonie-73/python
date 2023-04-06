9ee9b999c66413a7e79da58e4c149100c89fd7c3
Moonie-73 <khf@kenco.dk>
2023-04-06 20:49:08 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+"""Module descrption."""
+
+
+def main():
+    """Function description."""
+    aaa = 5
+    bbb = 3
+    ccc = aaa + bbb
+
+    print(f"{aaa} + {bbb} = {ccc}")
+
+
+if __name__ == "__main__":
+    main()
